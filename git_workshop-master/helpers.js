f45dba5cd165461d1efe5b63f53bc28bb3e3bd71
Guillermo Morales Marchebout <guillermo@Guillermos-MacBook-Pro.local>
2019-11-20 17:33:21 -0600

    order changed

--- git_workshop-master/helpers.js
+++ git_workshop-master/helpers.js
@@ -1,5 +1,5 @@
-const add = (number1, number2, number3) => {
-  return number1 + number2 + number3;
+const add = (number1, number2) => {
+  return number1 + number2 + number2;
 }
 
 const divide = (number1, number2) => {
@@ -7,7 +7,7 @@ const divide = (number1, number2) => {
 }
 
 const multiply = (number1, number2) => {
-  return 1;
+  return number2 * number1;
 }
 
 const substract = (number1, number2) => {
