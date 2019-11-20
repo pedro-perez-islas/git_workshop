780af5b2f77017df0cf97d2ba5c078d08a508eae
Guillermo Morales Marchebout <guillermo@Guillermos-MacBook-Pro.local>
2019-11-20 17:31:49 -0600

    adding a new number to calculate the total.

--- git_workshop-master/helpers.js
+++ git_workshop-master/helpers.js
@@ -1,5 +1,5 @@
-const add = (number1, number2) => {
-  return number1 + number2 + number2;
+const add = (number1, number2, number3) => {
+  return number1 + number2 + number3;
 }
 
 const divide = (number1, number2) => {
