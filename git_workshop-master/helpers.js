d9bc66142b1cd7aa49c14d48cec552b2bb9ce28a
Pedro Perez Islas <pedro.perez@wizeline.com>
2019-11-20 17:49:28 -0600

    Conflicts resolved

--- git_workshop-master/helpers.js
+++ git_workshop-master/helpers.js
@@ -7,11 +7,7 @@ const divide = (number1, number2) => {
 }
 
 const multiply = (number1, number2) => {
-<<<<<<< HEAD
   return number1 * number2 * number2;
-=======
-  return number2 * number1;
->>>>>>> master
 }
 
 const substract = (number1, number2) => {
